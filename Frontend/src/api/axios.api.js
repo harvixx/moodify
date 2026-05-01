bdd58a12885e60b4427c92f090ca2a43b1883f74
Harishlohar0 <harishlohar77777@gmail.com>
2026-05-01 12:46:06 +0530

    update

--- Frontend/src/api/axios.api.js
+++ Frontend/src/api/axios.api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://localhost:3002/api",
   withCredentials: true,
   timeout: 10000,
 });
