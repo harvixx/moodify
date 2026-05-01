f3107937e440ad638ef7d383fbb83b882a2fd3e1
Harishlohar0 <harishlohar77777@gmail.com>
2026-05-01 10:12:49 +0530

    update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const path = require("path");
 const authRouter = require("./routes/auth.routes");
 const songRouter = require("./routes/song.routes");
+
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: "http://localhost:5173", 
-        credentials: true, 
-    })
-);
+
+// API Routes
 app.use("/api/auth", authRouter);
-app.use("/api/songs",songRouter);
+app.use("/api/songs", songRouter);
+
+// Static files
+const frontendPath = path.join(__dirname, "../../Frontend/dist");
+app.use(express.static(frontendPath));
+
+// React routing
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
 module.exports = app;
